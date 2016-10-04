95559a58b9eb3e00bc2cb398646a2f156bbcbf17
Ben Pannell <bendpannell@gmail.com>
2016-10-03 23:15:35 -0400

    finally starting

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MMYS::Application.config.secret_key_base = '66d2e4956f150a05ed0ece4679af70a53c5f43acaa7d5b8b2e60e31ae3e5f7e4cfc44a0540e50fc5342406d7ad89678826252e9012fa7310b90192e50a0a0dbd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MMYS::Application.config.session_store :cookie_store, key: '_MMYS_session'
